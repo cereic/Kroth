1d1ee647df7039bdd9775304f3e0e61cdb3ea2c1
cereic <anthony.post@hotmail.com>
2013-02-02 12:07:15 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*************************
+Kroth PHP Framework
+
+** Requirements: PHP >= 5
+
+*/
+
+$main_folder = 'main';
+$apps_folder = 'apps';
+
+$constants = array('BASEDIR' => str_replace('\\', '/', realpath($main_folder)).'/',
+  				'APPSDIR' => str_replace( '\\', '/', realpath($apps_folder) ).'/');
+
+function defineConsts($input=NULL) {
+	global $constants;
+	
+	if ($input != NULL) {
+		foreach ($input as $constant => $value)
+			define($constant, $value);
+	}else {
+		foreach ($constants as $constant => $value)
+			define($constant, $value);
+	}
+}
+
+defineConsts();
+
+require BASEDIR . 'Boot.php';
